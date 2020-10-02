f5616f4f66db9d66f40862c2e7bf6cb1d0195dbf
Karan <karanbpawar@gmail.com>
2020-10-03 03:09:47 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,10 @@ import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
+
 void main() => runApp(MaterialApp(
-      home: Homescreen(),
-    ));
+  home: Homescreen(),
+));
 
 class Homescreen extends StatefulWidget {
   @override
@@ -36,13 +37,13 @@ class _HomeScreenState extends State<Homescreen> {
       //result = " ";
       final String text = label.text;
       final double confidence = label.confidence;
-        if(label.text=='Plant'){
-          setState(() {
-            result = 'Plant';
-          });
-        }
+      if (label.text == 'Plant') {
+        setState(() {
+          result = 'Plant';
+        });
+      }
     }
-    if(result=='Plant') {
+    if (result == 'Plant') {
       showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -58,7 +59,10 @@ class _HomeScreenState extends State<Homescreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Text('Excluding medical and asthetic values,\n69 iPhones 14Ducati Scrambler bikes\n2 Audis\n1 dream Apartment in Mumbai'),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text('This tree will Approximately: \n1)Releases water from ground each season enough to bath 2271 times.\n2)Absorb CO2: 450 km of average bike ride, 3832.5 hrs of laptop standby, 30 days of laptop on.\n\nDirect benefit(excluding medicinal and aesthetic values): \n1)69 iPhones, 14 Ducati Scrambler bikes, 2 Audis, 1 dream apartment in mumbai',style: TextStyle(fontSize: 23.0,fontFamily: 'SourceSansPro',color: Colors.teal.shade800),),
+                      ),
                       SizedBox(
                           width: 320.0,
                           child: FlatButton(
@@ -79,7 +83,7 @@ class _HomeScreenState extends State<Homescreen> {
             );
           });
     }
-    else if(result==" "){
+    else if (result == " ") {
       showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -90,23 +94,23 @@ class _HomeScreenState extends State<Homescreen> {
               child: Container(
                 height: 200,
                 child: Padding(
-                  padding: const EdgeInsets.all(12.0),
+                  padding: const EdgeInsets.all(10.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      TextField(
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Not a tree or a water body'),
-                      ),
+                      Container(height: 10.0,),
+                      Text('Not a tree!',style: TextStyle(fontSize: 23.0,fontFamily: 'SourceSansPro',color: Colors.teal.shade800),),
                       SizedBox(
                           width: 320.0,
                           child: FlatButton(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius:
-                                    BorderRadius.circular(10.0)),
-                            child: Text("Close",style: TextStyle(color: Colors.white),),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                BorderRadius.circular(10.0)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Text("Close",style: TextStyle(color: Colors.white),),
+                            ),
                             color: const Color(0xFF1BC0C5),
                             onPressed: (){
                               Navigator.of(context).pop();
@@ -142,9 +146,7 @@ class _HomeScreenState extends State<Homescreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('images/back.jpg'),
-              fit: BoxFit.cover
-          ),
+              image: AssetImage('images/back.jpg'), fit: BoxFit.cover),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -153,16 +155,16 @@ class _HomeScreenState extends State<Homescreen> {
             SizedBox(height: 150),
             isImageLoaded
                 ? Center(
-                    child: Container(
-                      height: 250.0,
-                      width: 250.0,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: FileImage(pickedImage), fit: BoxFit.cover)
-                      ),
-                    ),
-                  )
+              child: Container(
+                height: 250.0,
+                width: 250.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: FileImage(pickedImage),
+                        fit: BoxFit.cover)),
+              ),
+            )
                 : Container(),
             SizedBox(height: 30),
             Padding(
